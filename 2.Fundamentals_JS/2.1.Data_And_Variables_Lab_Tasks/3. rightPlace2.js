26a8054d9713b4988773c73c63fde73556f7e35d
AssiaIlieva <ac17@abv.bg>
2023-09-21 16:21:23 +0300

    Added Data and Variables Lab Tasks

---
+++ 2.Fundamentals_JS/2.1.Data_And_Variables_Lab_Tasks/3. rightPlace2.js
@@ -0,0 +1,6 @@
+function rightPlace(string, char, result){
+let newString = string.replace("_", char)
+let output = result === newString ? 'Matched' : 'Not Matched'
+console.log(output);
+}
+rightPlace('Str_ng', 'i', 'String')
