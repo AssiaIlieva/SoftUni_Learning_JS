4a6ac26c9e1987db92df68956f4d70ada54d844b
AssiaIlieva <ac17@abv.bg>
2023-09-24 11:35:42 +0300

    Create 10.triplesOfLatinLetters copy.js

--- 2.Fundamentals_JS/2.1.Data_And_Variables_Lab_Tasks/10.triplesOfLatinLetters.js
+++ 2.Fundamentals_JS/2.1.Data_And_Variables_Lab_Tasks/10.triplesOfLatinLetters.js
@@ -1,13 +1,13 @@
-function triplesOfLatinLetters(n) {
-  for (let i = 0; i < n; i++) {
-    for (let k = 0; k < n; k++) {
-      for (let j = 0; j < n; j++) {
-        let char1 = String.fromCharCode(97 + i);
-        let char2 = String.fromCharCode(97 + k);
-        let char3 = String.fromCharCode(97 + j);
-        console.log(`${char1}${char2}${char3}`);
-      }
+function triplesOfLatinLetters(n){
+for (let i = 0; i < n; i++){
+    for (let k = 0; k <  n; k++){
+        for (let j = 0; j <  n; j++){
+            let char1 = String.fromCharCode(97 + i);
+            let char2 = String.fromCharCode(97 + k);
+            let char3 = String.fromCharCode(97 + j);
+            console.log(`${char1}${char2}${char3}`);
+        }
     }
-  }
 }
-triplesOfLatinLetters(2);
+}
+triplesOfLatinLetters(2)
