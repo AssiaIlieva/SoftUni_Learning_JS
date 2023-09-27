db886eb442a6ad0e330e9b6e94ed95120f98d65a
AssiaIlieva <ac17@abv.bg>
2023-09-27 23:19:22 +0300

    Update 3.mergeArrays.js

--- 2.Fundamentals_JS/3.2.Arrays_Exercise_Tasks/3.mergeArrays.js
+++ 2.Fundamentals_JS/3.2.Arrays_Exercise_Tasks/3.mergeArrays.js
@@ -1,6 +1,5 @@
 function mergeArrays(arr1, arr2){
 let arrayLength = arr1.length;
-let result = [];
 
 for(let i = 0; i < arrayLength; i++){
     if(i % 2 == 0){
@@ -8,8 +7,7 @@ for(let i = 0; i < arrayLength; i++){
     }else{
         result[i] = arr1[i] + arr2[i];
     }
+  }
+  console.log(result.join(" - "));
 }
-console.log(result.join(' - '));
-}
-mergeArrays(['13', '12312', '5', '77', '4'],
-['22', '333', '5', '122', '44'] )
+mergeArrays(["13", "12312", "5", "77", "4"], ["22", "333", "5", "122", "44"]);
