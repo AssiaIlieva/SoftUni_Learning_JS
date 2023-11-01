3fc3e29c4e169c9eee61e35b0de2b2b9d286b7ce
AssiaIlieva <ac17@abv.bg>
2023-11-01 06:15:24 +0200

    Added a solution to Object and Classes More Exercise Tasks
    A second solution to Task 2. Flight Schedule

---
+++ 2.Fundamentals_JS/7.3.Objects_and_Classes_More_Exercise_Tasks/2.flightSchedule2.js
@@ -0,0 +1,49 @@
+function flightSchedule(input) {
+  let keyWord = input.pop()[0];
+  let flights = input[0];
+  let updatedFlights = input[1];
+  let objFlights = {};
+  for (let flight of flights) {
+    let [num, desination1, destination2] = flight.split(" ");
+    let destination = desination1 + " " + destination2;
+    if (!destination2) {
+      destination = desination1;
+    }
+    objFlights[num] = {
+      Destination: destination,
+      Status: "Ready to fly",
+    };
+  }
+  for (let flight of updatedFlights) {
+    let [num, status] = flight.split(" ");
+    if (objFlights.hasOwnProperty(num)) {
+      objFlights[num].Status = status;
+    }
+  }
+  for (let flight in objFlights) {
+    if (objFlights[flight].Status === keyWord) {
+      console.log(objFlights[flight]);
+    }
+  }
+}
+flightSchedule([
+  [
+    "WN269 Delaware",
+    "FL2269 Oregon",
+    "WN498 Las Vegas",
+    "WN3145 Ohio",
+    "WN612 Alabama",
+    "WN4010 New York",
+    "WN1173 California",
+    "DL2120 Texas",
+    "KL5744 Illinois",
+    "WN678 Pennsylvania",
+  ],
+  [
+    "DL2120 Cancelled",
+    "WN612 Cancelled",
+    "WN1173 Cancelled",
+    "SK330 Cancelled",
+  ],
+  ["Ready to fly"],
+]);
