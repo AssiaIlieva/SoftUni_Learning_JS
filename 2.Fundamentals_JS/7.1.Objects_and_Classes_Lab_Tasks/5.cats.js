40fe6099b8b42c8ff7214e56b17c9c20112f687d
AssiaIlieva <ac17@abv.bg>
2023-11-15 13:00:49 +0200

    Update of Object and Classes Lab Tasks
    Update of Task 5. Cats

--- 2.Fundamentals_JS/7.1.Objects_and_Classes_Lab_Tasks/5.cats.js
+++ 2.Fundamentals_JS/7.1.Objects_and_Classes_Lab_Tasks/5.cats.js
@@ -3,9 +3,16 @@ function catCreator(array){
         constructor(name, age){
             this.name = name,
             this.age = age 
-            this.meow = () => {
-                console.log(`${this.name}, age ${this.age} says Meow`);
             }
+        meow()  {
+            console.log(`${this.name}, age ${this.age} says Meow`);
         }
     }
+    for (let element of array){
+        let [cat, age] = element.split(' ');
+        let tempCat = new Cat(cat, age);
+        tempCat.meow();
+
+    }
 }
+catCreator(['Candy 1', 'Poppy 3', 'Nyx 2'])
