ba586011483c3bfce09d55ad1dd91d82b376a4f6
AssiaIlieva <ac17@abv.bg>
2023-12-05 01:25:16 +0200

    Added a solution in Final Exam Preparation Tasks
    The third solution to Task Activation keys

---
+++ 2.Fundamentals_JS/11.Final_Exam_Preparation_Tasks/1.activationKeys3.js
@@ -0,0 +1,52 @@
+function activationKeys(input) {
+  let key = input.shift();
+
+  let command = input.shift();
+
+  while (command !== "Generate") {
+    let tokens = command.split(">>>");
+    let action = tokens[0];
+    if (action === "Contains") {
+      let substring = tokens[1];
+      key.includes(substring)
+        ? console.log(`${key} contains ${substring}`)
+        : console.log(`Substring not found!`);
+    } else if (action === "Flip") {
+      let caseType = tokens[1];
+      let startInx = Number(tokens[2]);
+      let endInx = Number(tokens[3]);
+      let substring = key.slice(startInx, endInx);
+      let leftHalf = key.slice(0, startInx);
+      let rightHalf = key.slice(endInx);
+      if (caseType === "Upper") {
+        substring = substring.toUpperCase();
+        key = leftHalf + substring + rightHalf;
+        console.log(key);
+      } else if (caseType === "Lower") {
+        substring = substring.toLowerCase();
+        key = leftHalf + substring + rightHalf;
+        console.log(key);
+      }
+    } else if (action === "Slice") {
+      let startInx = Number(tokens[1]);
+      let endInx = Number(tokens[2]);
+      let substring = key.slice(startInx, endInx);
+      let leftHalf = key.slice(0, startInx);
+      let rightHalf = key.slice(endInx);
+      key = leftHalf + rightHalf;
+      console.log(key);
+    }
+
+    command = input.shift();
+  }
+  console.log(`Your activation key is: ${key}`);
+}
+activationKeys([
+  "abcdefghijklmnopqrstuvwxyz",
+  "Slice>>>2>>>6",
+  "Flip>>>Upper>>>3>>>14",
+  "Flip>>>Lower>>>5>>>7",
+  "Contains>>>def",
+  "Contains>>>deF",
+  "Generate",
+]);
