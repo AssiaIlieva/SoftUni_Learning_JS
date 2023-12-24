8f81018da83a5ecc35ac5e09453d6a8cd39ebaa4
AssiaIlieva <ac17@abv.bg>
2023-12-24 12:07:17 +0200

    Added a solution in Final Exam Preparation Tasks
    The first solution to Task 1. World Tour

---
+++ 2.Fundamentals_JS/11.Final_Exam_Preparation_Tasks/1.worldTour1.js
@@ -0,0 +1,45 @@
+function worldTour(input) {
+  let travelPlan = input.shift();
+
+  let command = input.shift().split(":");
+
+  while (command[0] !== "Travel") {
+    if (command[0] === "Add Stop") {
+      let index = command[1];
+      let stop = command[2];
+      if (index >= 0 && index < travelPlan.length) {
+        let firstPart = travelPlan.substring(0, index);
+        let secondPart = travelPlan.substring(index);
+        travelPlan = firstPart + stop + secondPart;
+      }
+    } else if (command[0] === "Remove Stop") {
+      let startIndex = Number(command[1]);
+      let endIndex = Number(command[2]);
+      if (
+        startIndex >= 0 &&
+        startIndex < travelPlan.length &&
+        endIndex >= 0 &&
+        endIndex < travelPlan.length
+      ) {
+        let firstPart = travelPlan.substring(0, startIndex);
+        let secondPart = travelPlan.substring(endIndex + 1);
+        travelPlan = firstPart + secondPart;
+      }
+    } else if (command[0] === "Switch") {
+      let oldString = command[1];
+      let newString = command[2];
+      if (travelPlan.includes(oldString)) {
+        let splitTravelPlan = travelPlan.split(oldString);
+        travelPlan = splitTravelPlan.join(newString);
+      }
+}
+    console.log(travelPlan);
+    command = input.shift().split(":");
+  }
+  console.log(`Ready for world tour! Planned stops: ${travelPlan}`);
+}
+worldTour(["Hawai::Cyprys-Greece",
+"Add Stop:7:Rome",
+"Remove Stop:11:16",
+"Switch:Hawai:Bulgaria",
+"Travel"]);
