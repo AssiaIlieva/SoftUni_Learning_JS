c3b50aa3c7f21f9c21ae15a26017d8ed52361948
AssiaIlieva <ac17@abv.bg>
2023-12-26 21:38:16 +0200

    Added a solution in Final Exam Preparation Tasks
    The second solution to Task 2.Destination Mapper

---
+++ 2.Fundamentals_JS/11.Final_Exam_Preparation_Tasks/2.destinationMapper2.js
@@ -0,0 +1,20 @@
+function destinationMapper(input) {
+  let regex = /[=\/][A-Z][A-Za-z]{2,}[=\/]/gm;
+  let points = 0;
+  let destination = [];
+
+  let foundDestination = input.match(regex);
+
+  if (foundDestination) {
+    foundDestination.forEach((element) => {
+      if (element[0] === element[element.length - 1]) {
+        element = element.substring(1, element.length - 1);
+        destination.push(element);
+        points += element.length;
+      }
+    });
+  }
+  console.log(`Destinations: ${destination.join(", ")}`);
+  console.log(`Travel Points: ${points}`);
+}
+destinationMapper("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=");
