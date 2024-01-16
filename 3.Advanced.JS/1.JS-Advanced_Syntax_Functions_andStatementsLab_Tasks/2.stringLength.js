7fbad334c1b10cc6a2311f24aae4ec9f1490e015
AssiaIlieva <ac17@abv.bg>
2024-01-16 07:37:38 +0200

    Added a solution in JS Advanced Syntax Functions and Statements Lab Tasks
    A solution to Task 2. String Length

---
+++ 3.Advanced.JS/1.JS-Advanced_Syntax_Functions_andStatementsLab_Tasks/2.stringLength.js
@@ -0,0 +1,7 @@
+function stringLenght(stirng1, string2, string3){
+let sum = stirng1.length + string2.length + string3.length;
+let average = Math.floor(sum / 3);
+console.log(sum);
+console.log(average);
+}
+stringLenght('pasta', '5', '22.3')
