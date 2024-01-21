6f0934d400a3a6efe33e34901cb96256ae21738f
AssiaIlieva <ac17@abv.bg>
2024-01-21 06:19:27 +0200

    Added a solution in Syntax Functions and Statements Lab
    The solution to Task 7. Day of Week

---
+++ 3.Advanced.JS/1.JS-Advanced_Syntax_Functions_andStatementsLab_Tasks/7.dayOfWeek.js
@@ -0,0 +1,30 @@
+function dayOfWeek(input) {
+  let result = "error";
+
+  switch (input) {
+    case "Monday":
+      result = 1;
+      break;
+    case "Tuesday":
+      result = 2;
+      break;
+    case "Wednesday":
+      result = 3;
+      break;
+    case "Thursday":
+      result = 4;
+      break;
+    case "Friday":
+      result = 5;
+      break;
+    case "Saturday":
+      result = 6;
+      break;
+    case "Sunday":
+      result = 7;
+      break;
+  }
+
+  console.log(result);
+}
+dayOfWeek("Invalid");
