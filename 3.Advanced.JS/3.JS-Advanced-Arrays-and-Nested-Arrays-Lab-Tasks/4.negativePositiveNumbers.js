0bd836ac70ccd6c0d4f3640af06ef449c63865d3
AssiaIlieva <ac17@abv.bg>
2024-02-07 05:42:05 +0200

    Added a solution in Arrays and Nested Arrays Lab Tasks
    The solution to Task 4. Negative Positive Numbers

---
+++ 3.Advanced.JS/3.JS-Advanced-Arrays-and-Nested-Arrays-Lab-Tasks/4.negativePositiveNumbers.js
@@ -0,0 +1,12 @@
+function negativePositiveNumbers(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < 0) {
+      result.unshift(array[i]);
+    } else {
+      result.push(array[i]);
+    }
+  }
+  console.log(result.join("\n"));
+}
+negativePositiveNumbers([3, -2, 0, -1]);
