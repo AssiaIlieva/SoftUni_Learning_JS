aa8a4c5f625a5f77d325ac01825ef8f7fbccc054
AssiaIlieva <ac17@abv.bg>
2024-04-13 10:17:35 +0300

    Added Task in Asynchronous Programming Exercise Tasks
    The resources of Task 1.Bus stop

--- 2.Fundamentals_JS/9.2.Text_Processing_Exercise_Tasks/3.extractFile1.js
+++
@@ -1,9 +0,0 @@
-function extractFile(address) {
-  let fileInfo = address.split("\\").pop();
-  let dotIndex = fileInfo.lastIndexOf('.');
-  let fileExtention = fileInfo.slice(dotIndex + 1);
-  let fileName = fileInfo.slice(0, dotIndex);
-  console.log(`File name: ${fileName}`);
-  console.log(`File extension: ${fileExtention}`);
-}
-extractFile("C:\\Internal\\training-internal\\Template.pptx");
